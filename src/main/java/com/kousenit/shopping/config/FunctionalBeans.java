b5f863fde60be929b624eb2cb5d2be363427ea40
Ken Kousen <ken.kousen@kousenit.com>
2022-11-09 23:30:17 -0500

    Upgrade dependencies

--- src/main/java/com/kousenit/shopping/config/FunctionalBeans.java
+++ src/main/java/com/kousenit/shopping/config/FunctionalBeans.java
@@ -14,6 +14,7 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 public class FunctionalBeans {
 
     // Alternative CommandLineRunner (replaces the AppInit class)
+    // @Bean
 //    public CommandLineRunner initialize(@Autowired ProductService service) {
 //        return args -> service.initializeDatabase();
 //    }
